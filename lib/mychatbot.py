49d82af4e722fcde9bb92727b22e17a81a6bc0af
Larry Cohen <cohlar@gmail.com>
2020-01-08 15:41:07 +0200

    bug fix

--- lib/mychatbot.py
+++ lib/mychatbot.py
@@ -1,24 +1,23 @@
 from . import utils, rules
 
-DEFAULT_ANIMATION = 'confused'
-DEFAULT_MSG = '''Sorry, not sure what to say. I'm not that smart: please try to rephrase. Please note that I speak only English at the moment'''
+default_animation = 'confused'
+default_msg = '''Sorry, not sure what to say. I'm not that smart: please try to rephrase. Please note that I speak only English at the moment'''
 
-SWEAR_ANIMATION = 'heartbroke'
-SWEAR_MESSAGE = 'This is not an appropriate language, please watch your tongue!'
+swear_animation = 'heartbroke'
+swear_message = 'This is not an appropriate language, please watch your tongue!'
 
 rules = rules.set_rules()
-swears = utils.get_swears()
+swears = utils.get_swears()    
 
 def process_user_message(message):
 
     user_message = message.lower()
 
-    response = utils.set_response(DEFAULT_ANIMATION, DEFAULT_MSG)
-    print('current_time')
+    response = utils.set_response(default_animation, default_msg)
 
     for swear in swears:
         if swear in user_message:
-            utils.set_response(SWEAR_ANIMATION, SWEAR_MESSAGE, response)
+            utils.set_response(swear_animation, swear_message, response)
             return response
 
     for rule in sorted(rules, key=utils.get_accuracy_level):
@@ -33,8 +32,7 @@ def process_user_message(message):
                 type(keyword) is str and keyword in user_message
             ):
                 animation = rule['animation']
-                msg = rule['msg']() if type(
-                    rule['msg']()) is 'function' else rule['msg']()
+                msg = rule['msg']() if callable(rule['msg']) else rule['msg']
                 utils.set_response(animation, msg, response)
                 return response
 
